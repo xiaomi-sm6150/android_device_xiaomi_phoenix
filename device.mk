e27a8fd6f2c250395ad3ec04a0bf4a16f9e51a6f
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 16:25:29 -0700

    phoenix: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3

--- device.mk
+++ device.mk
@@ -90,9 +90,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
