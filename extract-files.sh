f8f23169f606eb076321e2e4ac400cc5157b3b81
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    phoenix: Rename camera face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837

--- extract-files.sh
+++ extract-files.sh
@@ -11,6 +11,11 @@ function blob_fixup() {
         vendor/lib64/libgoodixhwfingerprint.so )
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        vendor/lib64/hw/camera.qcom.so | vendor/lib64/libmegface.so | vendor/lib64/libFaceDetectpp-0.5.2.so)
+            sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
+            sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
+            sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
+            ;;
     esac
 }
 
